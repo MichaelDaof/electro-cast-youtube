218fa6f1bc9888de73bfa24f5680c60c93624133
Michael Daof <michael.ivanhoe@gmail.com>
2017-08-17 12:23:53 -0700

    youtube API enabled

--- config/handlers/query.js
+++ config/handlers/query.js
@@ -4,12 +4,24 @@
  * 
  * This is basically a Google Developer API key-enabled redirect.
  */
-const key = process.env.YOUTUBE_DEV_KEY || require('../../key.json').key;
+
+const ytSearch = require('youtube-search');
+
+let key;
+try {
+  key = process.env.YOUTUBE_DEV_KEY || require('../../key.json').key;
+} catch(e) {
+  key = key || 'dud';
+  console.error('You don\'t have a valid source for your developer key. Check your process environment, or if developing locally, you\'ll have to create that file yourself')
+  throw e
+}
 
 function query(req, res) {
   const search = req.params.search;
-  console.log(`q=${search} and ${key}`)
-  res.send(`got ${search}`)
+  ytSearch(search, { maxResults: 20, key }, function(err, result) {
+    console.log('results: ', result, err)
+    res.json(result)
+  })
 }
 
 module.exports = query;
--- config/handlers/readyState.js
+++ config/handlers/readyState.js
@@ -1,10 +1,12 @@
 /**
  * Check for service ready state and provide any default configs
  * for YouTube API queries
+ * 
+ * We're waking up our free Heroku server.
  */
 
 function readyState(req, res) {
-  res.send('I\'m ready! And I got some configuration data for you!')
+  res.json({ up: true })
 }
 
 module.exports = readyState;
